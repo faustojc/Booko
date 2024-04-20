c193a69d0f009cc1ed4eca52c6ea70e62aa404c0
faustojc <faustojcboko@gmail.com>
2024-04-20 20:01:39 +0800

    Adjust: Refactor for optimizations

--- lib/domain/repository/user/user_repo.dart
+++ lib/domain/repository/user/user_repo.dart
@@ -5,19 +5,32 @@ import 'package:booko/domain/repository/auth/auth_repo.dart';
 class UserRepo {
   final AuthRepo _authRepo;
 
-  late Customer customer;
+  late Customer? customer;
 
   final List<Ticket> tickets = [];
 
   UserRepo({required AuthRepo authRepo}) : _authRepo = authRepo;
-  
+
   Future<void> fetchData() async {
-    customer = (await Customer().where(field: 'user_id', operator: '==', value: _authRepo.currentUser!.uid).get()).first;
-    tickets.addAll((await Ticket().where(field: 'customer_id', operator: '==', value: _authRepo.currentUser!.uid).get()).toList());
+    customer ??= (await Customer().where(field: 'user_id', operator: '==', value: _authRepo.currentUser!.uid).get()).first;
+
+    if (tickets.isEmpty) {
+      tickets.addAll((await Ticket().where(field: 'user_id', operator: '==', value: _authRepo.currentUser!.uid).get()).toList());
+    }
   }
 
   Future<void> createCustomer({required String firstname, required String lastname, required String birthday}) async {
     await Customer().insert({'firstname': firstname, 'lastname': lastname, 'birthday': birthday, 'user_id': _authRepo.currentUser!.uid});
-    await fetchData();
+
+    final json = {
+      'user_id': _authRepo.currentUser!.uid,
+      'firstname': firstname,
+      'lastname': lastname,
+      'birthday': birthday,
+      'created_at': DateTime.now(),
+      'updated_at': DateTime.now(),
+    };
+
+    customer = Customer.fromJson(json);
   }
 }
