c3715699dcb8546a985db48cbd4654dc73169077
Kielzu <112738101+Kielzu@users.noreply.github.com>
2024-05-05 22:48:25 +0800

    QR Code Update

--- lib/presentation/widget/seat/buy_ticket_button.dart
+++ lib/presentation/widget/seat/buy_ticket_button.dart
@@ -1,5 +1,6 @@
 import 'package:booko/presentation/bloc/seat/seat_cubit.dart';
 import 'package:booko/resources/colors/theme_colors.dart';
+import 'package:booko/presentation/widget/seat/qr_code_generator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -26,25 +27,32 @@ class BuyTicketButton extends StatelessWidget {
                 padding: EdgeInsets.symmetric(vertical: 12),
                 child: CircularProgressIndicator(color: ThemeColor.white),
               ));
-        } else if (seatCubit.seatRepo.selectedSchedule == null || seatCubit.seatRepo.selectedSeats.isEmpty) {
+        } else if (seatCubit.seatRepo.selectedSchedule == null ||
+            seatCubit.seatRepo.selectedSeats.isEmpty) {
           return ElevatedButton(
             onPressed: () {},
             style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(ThemeColor.primary.withOpacity(0.4)),
+              backgroundColor: MaterialStateProperty.all(
+                  ThemeColor.primary.withOpacity(0.4)),
               shape: MaterialStateProperty.all(const RoundedRectangleBorder(
                 borderRadius: BorderRadius.all(Radius.circular(14)),
               )),
             ),
             child: Padding(
               padding: const EdgeInsets.symmetric(vertical: 12),
-              child: Text('Buy Ticket', style: TextStyle(color: Colors.white.withOpacity(0.4), fontSize: 20)),
+              child: Text('Buy Ticket',
+                  style: TextStyle(
+                      color: Colors.white.withOpacity(0.4), fontSize: 20)),
             ),
           );
         }
 
         return ElevatedButton(
           onPressed: () {
-            // TODO: Display the ticket with generated QR code
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                  builder: (context) => QrCodeGenerator(data: ['Pew'])),
+            );
           },
           style: ButtonStyle(
             backgroundColor: MaterialStateProperty.all(ThemeColor.primary),
@@ -54,7 +62,8 @@ class BuyTicketButton extends StatelessWidget {
           ),
           child: const Padding(
             padding: EdgeInsets.symmetric(vertical: 12),
-            child: Text('Buy Ticket', style: TextStyle(color: Colors.white, fontSize: 20)),
+            child: Text('Buy Ticket',
+                style: TextStyle(color: Colors.white, fontSize: 20)),
           ),
         );
       },
